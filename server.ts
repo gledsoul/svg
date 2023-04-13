069507b0315ab3b1acab8d470367393c5f3a27e8
Gledson Joaquim Lopes <gledsonjoaquimlopes@Gledsons-MacBook-Pro.local>
2023-04-13 20:12:58 +0100

    dynamic support

--- server.ts
+++ server.ts
@@ -3,9 +3,20 @@ import react from "aleph/plugins/react";
 import unocss from "aleph/plugins/unocss";
 import config from "./unocss.config.ts";
 
+
+import routes from "./routes/_export.ts";
+
+
+
 serve({
   plugins: [
     react({ ssr: true }),
     unocss(config),
   ],
+  router:{
+    routes,
+    glob: "./routes/**/*.{ts,tsx}",
+	  
+  }
+
 });
